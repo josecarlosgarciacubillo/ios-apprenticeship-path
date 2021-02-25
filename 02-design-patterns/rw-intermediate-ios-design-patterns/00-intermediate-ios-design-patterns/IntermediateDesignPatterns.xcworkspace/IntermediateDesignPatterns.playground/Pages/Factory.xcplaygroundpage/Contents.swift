2175f019b98611da59c784dd77e390264f756f73
José Carlos García Cubillo <carlos.garcia@wizeline.com>
2021-02-25 11:46:26 -0600

    - MVVM, Factory, Adapter, and Iterator.

--- 02-design-patterns/rw-intermediate-ios-design-patterns/00-intermediate-ios-design-patterns/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
+++ 02-design-patterns/rw-intermediate-ios-design-patterns/00-intermediate-ios-design-patterns/IntermediateDesignPatterns.xcworkspace/IntermediateDesignPatterns.playground/Pages/Factory.xcplaygroundpage/Contents.swift
@@ -12,3 +12,59 @@
  
  ## Code Example
  */
+import Foundation
+
+public struct JobApplicant {
+  public enum Status {
+    case new, interview, hired, rejected
+  }
+  public let name: String
+  public let email: String
+  public var status: Status
+}
+
+public struct Email {
+  public let subject: String
+  public let messageBody: String
+  public let recipientemail: String
+  public let senderEmail: String
+}
+
+public struct EmailFactory {
+  public let senderEmail: String
+  
+  public func createEmail(to recipient: JobApplicant) -> Email {
+    let subject: String
+    let messageBody: String
+    switch recipient.status {
+    case .new:
+      subject = "We Received Your Application"
+      messageBody = "Thanks for applying for a job here! " +
+                    "You should hear from us in 17-42 business days."
+    case .interview:
+      subject = "We Want To Interview You"
+      messageBody = "Thanks for your resume, \(recipient.name)! " +
+                    "Can you come in for an interview in 30 minutes?"
+    case .hired:
+      subject = "We Want To Hire You"
+      messageBody = "Congratulations, \(recipient.name)! " +
+                    "We liked your code, and you smelled nice, " +
+                    "We want to offer you a position! Cha-chang! $$$"
+    case .rejected:
+      subject = "Thanks For Your Application"
+      messageBody = "Thank you for applying, \(recipient.name)! " +
+                    "We have decided to move forward with other candidates. " +
+                    "Please remember to weat pants next time!"
+    }
+    return Email(subject: subject, messageBody: messageBody, recipientemail: recipient.email, senderEmail: senderEmail)
+  }
+}
+
+// MARK: - Example
+var garcia = JobApplicant(name: "José Carlos García", email: "jose@carlosgarcia.com", status: .new)
+let emailFactory = EmailFactory(senderEmail: "sergey@google.com")
+print(emailFactory.createEmail(to: garcia), "\n")
+garcia.status = .interview
+print(emailFactory.createEmail(to: garcia), "\n")
+garcia.status = .hired
+print(emailFactory.createEmail(to: garcia), "\n")
