4d01e6f2006cc38e49811c2bcb2ea4be63a47560
José Carlos García Cubillo <carlos.garcia@wizeline.com>
2021-03-02 08:59:09 -0600

    Codable.

---
+++ 03-swift-language/02-codable/Codable.playground/Contents.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+struct Coordinate {
+  var latitude: Double
+  var longitude: Double
+  var elevation: Double
+  
+  enum CodingKeys: String, CodingKey {
+    case latitude
+    case longitude
+    case additionalInfo
+  }
+  
+  enum AdditionalInfoKeys: String, CodingKey {
+    case elevation
+  }
+}
+
+extension Coordinate: Decodable {
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    latitude = try values.decode(Double.self, forKey: .latitude)
+    longitude = try values.decode(Double.self, forKey: .longitude)
+    
+    let additionalInfo = try values.nestedContainer(keyedBy: AdditionalInfoKeys.self, forKey: .additionalInfo)
+    elevation = try additionalInfo.decode(Double.self, forKey: .elevation)
+  }
+}
+
+extension Coordinate: Encodable {
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+    try container.encode(latitude, forKey: .latitude)
+    try container.encode(longitude, forKey: .longitude)
+    
+    var additionalInfo = container.nestedContainer(keyedBy: AdditionalInfoKeys.self, forKey: .additionalInfo)
+    try additionalInfo.encode(elevation, forKey: .elevation)
+  }
+}
